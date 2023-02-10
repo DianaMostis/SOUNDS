be2ff32157f19ebb7a94f241daf298b6043eaa9f
hajmub <83780326+hajmub@users.noreply.github.com>
2023-02-10 09:52:29 +0000

    Add files via upload

--- sound.js
+++ sound.js
@@ -1,23 +1,23 @@
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
-const button4 = document.querySelector('#button4');
-const button5 = document.querySelector('#button5');
-const button6 = document.querySelector('#button6');
-const button7 = document.querySelector('#button7');
-const button8 = document.querySelector('#button8');
-const button9 = document.querySelector('#button9');
-const button10 = document.querySelector('#button10');
-const button11 = document.querySelector('#button11');
-const button12 = document.querySelector('#button12');
-const button13 = document.querySelector('#button13');
-const button14 = document.querySelector('#button14');
-const button15 = document.querySelector('#button15');
-const button16 = document.querySelector('#button16');
-const button17 = document.querySelector('#button17');
-const button18 = document.querySelector('#button18');
-const button19 = document.querySelector('#button19');
-const button20 = document.querySelector('#button20');
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const button7 = document.querySelector("#button7");
+const button8 = document.querySelector("#button8");
+const button9 = document.querySelector("#button9");
+const button10 = document.querySelector("#button10");
+const button11 = document.querySelector("#button11");
+const button12 = document.querySelector("#button12");
+const button13 = document.querySelector("#button13");
+const button14 = document.querySelector("#button14");
+const button15 = document.querySelector("#button15");
+const button16 = document.querySelector("#button16");
+const button17 = document.querySelector("#button17");
+const button18 = document.querySelector("#button18");
+const button19 = document.querySelector("#button19");
+const button20 = document.querySelector("#button20");
 
 let audio1;
 let audio2;
@@ -40,140 +40,168 @@ let audio18;
 let audio19;
 let audio20;
 
-button1.addEventListener('click', function() {
+button1.addEventListener("click", function () {
   if (!audio1 || audio1.ended) {
-    audio1 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1939-%20frankly%20my%20dear%20i%20dont%20give%20a%20damn.mp3?raw=true');
+    audio1 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1939-%20frankly%20my%20dear%20i%20dont%20give%20a%20damn.mp3?raw=true"
+    );
     audio1.play();
   }
 });
 
-button2.addEventListener('click', function() {
+button2.addEventListener("click", function () {
   if (!audio2 || audio2.ended) {
-    audio2 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1939%20Somewhere%20Over%20the%20Rainbow%20%20The%20Wizard%20of%20Oz%2018%20Movie%20CLIP.mp3?raw=true');
+    audio2 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1939%20Somewhere%20Over%20the%20Rainbow%20%20The%20Wizard%20of%20Oz%2018%20Movie%20CLIP.mp3?raw=true"
+    );
     audio2.play();
   }
 });
 
-button3.addEventListener('click', function() {
+button3.addEventListener("click", function () {
   if (!audio3 || audio3.ended) {
-    audio3 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1939%20SING%20SING%20SING%20BY%20BENNY%20GOODMAN-.mp3?raw=true');
+    audio3 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1939%20SING%20SING%20SING%20BY%20BENNY%20GOODMAN-.mp3?raw=true"
+    );
     audio3.play();
   }
 });
 
-button4.addEventListener('click', function() {
+button4.addEventListener("click", function () {
   if (!audio4 || audio4.ended) {
-    audio4 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1931%20-%20%20Its%20Alive!%20-%20Frankenstein.mp3?raw=true');
+    audio4 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1931%20-%20%20Its%20Alive!%20-%20Frankenstein.mp3?raw=true"
+    );
     audio4.play();
   }
 });
-button5.addEventListener('click', function() {
+button5.addEventListener("click", function () {
   if (!audio5 || audio5.ended) {
-    audio3 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1940-Winston%20Churchill%20%20We%20shall%20never%20surrender-speech.mp3?raw=true');
+    audio3 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1940-Winston%20Churchill%20%20We%20shall%20never%20surrender-speech.mp3?raw=true"
+    );
     audio3.play();
   }
 });
-button6.addEventListener('click', function() {
+button6.addEventListener("click", function () {
   if (!audio6 || audio6.ended) {
-    audio6 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1939%20Ella%20Fitzgerald_%20Puttin%20On%20The%20Ritz.mp3?raw=true');
+    audio6 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1939%20Ella%20Fitzgerald_%20Puttin%20On%20The%20Ritz.mp3?raw=true"
+    );
     audio6.play();
   }
 });
-button7.addEventListener('click', function() {
+button7.addEventListener("click", function () {
   if (!audio7 || audio7.ended) {
-    audio7 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1950-Elvis%20Presley%20-%20Tutti%20Frutti.mp3?raw=true');
+    audio7 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1950-Elvis%20Presley%20-%20Tutti%20Frutti.mp3?raw=true"
+    );
     audio7.play();
   }
 });
-button8.addEventListener('click', function() {
+button8.addEventListener("click", function () {
   if (!audio8 || audio8.ended) {
-    audio8 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1952%20-%20Singing%20in%20the%20Rain.mp3?raw=true');
+    audio8 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1952%20-%20Singing%20in%20the%20Rain.mp3?raw=true"
+    );
     audio8.play();
   }
 });
-button9.addEventListener('click', function() {
+button9.addEventListener("click", function () {
   if (!audio9 || audio9.ended) {
-    audio9 = new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1966-%20The%20Beach%20Boys%20%20Good%20Vibrations-.mp3?raw=true');
+    audio9 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1966-%20The%20Beach%20Boys%20%20Good%20Vibrations-.mp3?raw=true"
+    );
     audio9.play();
   }
 });
-button10.addEventListener('click', function() {
+button10.addEventListener("click", function () {
   if (!audio10 || audio10.ended) {
-    audio10= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1964%20-%20Mary%20Poppins%20-%20Supercalifragilisticexpialidocious.mp3?raw=true');
+    audio10 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1964%20-%20Mary%20Poppins%20-%20Supercalifragilisticexpialidocious.mp3?raw=true"
+    );
     audio10.play();
   }
 });
-button11.addEventListener('click', function() {
+button11.addEventListener("click", function () {
   if (!audio11 || audio11.ended) {
-    audio11= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1963%20-%20JF%20KENNEDY%20_FREEDOM%20SPEECH-BERLIN%20WALL.mp3?raw=true');
+    audio11 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1963%20-%20JF%20KENNEDY%20_FREEDOM%20SPEECH-BERLIN%20WALL.mp3?raw=true"
+    );
     audio11.play();
   }
 });
-button12.addEventListener('click', function() {
+button12.addEventListener("click", function () {
   if (!audio12 || audio12.ended) {
-    audio12= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1975%20-%20Queen%20%E2%80%93%20Bohemian%20Rhapsody.mp3?raw=true');
+    audio12 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1975%20-%20Queen%20%E2%80%93%20Bohemian%20Rhapsody.mp3?raw=true"
+    );
     audio12.play();
   }
 });
-button13.addEventListener('click', function() {
+button13.addEventListener("click", function () {
   if (!audio13 || audio13.ended) {
-    audio13= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1980-%20PacMan%20Original%20Theme.mp3?raw=true');
+    audio13 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1980-%20PacMan%20Original%20Theme.mp3?raw=true"
+    );
     audio13.play();
   }
 });
-button14.addEventListener('click', function() {
+button14.addEventListener("click", function () {
   if (!audio14 || audio14.ended) {
-    audio14= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1982%20-%20ET%20Phone%20Home%20%20ET%20The%20ExtraTerrestrial.mp3?raw=true');
+    audio14 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1982%20-%20ET%20Phone%20Home%20%20ET%20The%20ExtraTerrestrial.mp3?raw=true"
+    );
     audio14.play();
   }
 });
-button15.addEventListener('click', function() {
+button15.addEventListener("click", function () {
   if (!audio15 || audio15.ended) {
-    audio15= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1991-%20Terminator%202%20-%20Hasta%20La%20Vista%20Baby.mp3?raw=true');
+    audio15 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1991-%20Terminator%202%20-%20Hasta%20La%20Vista%20Baby.mp3?raw=true"
+    );
     audio15.play();
   }
 });
-button16.addEventListener('click', function() {
-  if (!audio16|| audio16.ended) {
-    audio16= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/1997%20-%20Backstreet%20Boys%20-%20Everybody.mp3?raw=true');
+button16.addEventListener("click", function () {
+  if (!audio16 || audio16.ended) {
+    audio16 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/1997%20-%20Backstreet%20Boys%20-%20Everybody.mp3?raw=true"
+    );
     audio16.play();
   }
 });
-button17.addEventListener('click', function() {
+button17.addEventListener("click", function () {
   if (!audio17 || audio17.ended) {
-    audio17= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/2000-%20Stephen%20Hawking%20-%20No%20limits%20to%20the%20human%20spirit%20-%20Copy%20-%20Copy.mp3?raw=true');
+    audio17 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/2000-%20Stephen%20Hawking%20-%20No%20limits%20to%20the%20human%20spirit%20-%20Copy%20-%20Copy.mp3?raw=true"
+    );
     audio17.play();
   }
 });
 
-button18.addEventListener('click', function() {
+button18.addEventListener("click", function () {
   if (!audio18 || audio18.ended) {
-    audio18= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/2000-%20Leviosa%20Not%20Leviosaaa%20%20%20Harry%20Potter.mp3?raw=true');
+    audio18 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/2000-%20Leviosa%20Not%20Leviosaaa%20%20%20Harry%20Potter.mp3?raw=true"
+    );
     audio18.play();
   }
 });
-button19.addEventListener('click', function() {
+button19.addEventListener("click", function () {
   if (!audio19 || audio19.ended) {
-    audio19= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/2006-bob%20sinclar%20everybody%20dance%20now.mp3?raw=true');
+    audio19 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/2006-bob%20sinclar%20everybody%20dance%20now.mp3?raw=true"
+    );
     audio19.play();
   }
 });
 
-button20.addEventListener('click', function() {
+button20.addEventListener("click", function () {
   if (!audio20 || audio20.ended) {
-    audio20= new Audio('https://github.com/DianaMostis/SOUNDS/blob/main/2013%20-%20%20FROZEN%20_%20Let%20It%20Go%20Sing.mp3?raw=true');
+    audio20 = new Audio(
+      "https://github.com/DianaMostis/SOUNDS/blob/main/2013%20-%20%20FROZEN%20_%20Let%20It%20Go%20Sing.mp3?raw=true"
+    );
     audio20.play();
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
